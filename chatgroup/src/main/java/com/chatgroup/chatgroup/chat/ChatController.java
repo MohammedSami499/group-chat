6261647f73ddb68c30ec46eb3544076b985c5af7
Mohammed Sami <76439631+MohammedSami499@users.noreply.github.com>
2024-03-11 16:30:20 +0200

    Update ChatController.java

--- chatgroup/src/main/java/com/chatgroup/chatgroup/chat/ChatController.java
+++ chatgroup/src/main/java/com/chatgroup/chatgroup/chat/ChatController.java
@@ -8,21 +8,6 @@ import org.springframework.stereotype.Controller;
 
 @Controller
 public class ChatController {
-//
-//    @MessageMapping("/chat.sendMessage")
-//    @SendTo("/topic/public")
-//    public ChatMessage sendMessage (@Payload ChatMessage chatMessage){
-//        return chatMessage;
-//    }
-//
-//    @MessageMapping("/chat.sendMessage")
-//    @SendTo("/topic/public")
-//    public ChatMessage addUser(@Payload ChatMessage chatMessage ,
-//                               SimpMessageHeaderAccessor headerAccessor
-//                               ){
-//        headerAccessor.getSessionAttributes().put("usename " , chatMessage.getSender());
-//        return chatMessage;
-//    }
 
     @MessageMapping("/chat.sendMessage")
     @SendTo("/topic/public")
